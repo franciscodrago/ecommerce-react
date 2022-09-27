56dee3a6cd20a1bd5d44f67b625921713121bc71
francisco_drago <franciscodrago1999@gmail.com>
2022-09-27 19:01:13 -0300

    Agregar navbar

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import './Navbar.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const Navbar = () => {
+    return (
+        <nav>
+<ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled">Disabled</a>
+  </li>
+</ul>
+        </nav>
+    )
+}
+
+export default Navbar
